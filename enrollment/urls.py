256b910e8a61c1786b9ab85a38153bba0fe942c0
sshhaayyuuee <s.shayue@gmail.com>
2014-05-19 18:19:47 +0800

    Revert "鍒涘缓2014.5.19"
    This reverts commit ba16e427ed2644664cb409408d2d16eccdbcb606.
    
    Conflicts:
    	db.sqlite3
    	enrollment/models.py
    	enrollment/views.py

--- enrollment/urls.py
+++ enrollment/urls.py
@@ -4,6 +4,5 @@ from enrollment import views
 
 urlpatterns = patterns('',
     url(r'^$', views.IndexView.as_view(),name='index'),
-    url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
-    url(r'^add/(?P<id>\d+)/$',views.StuCreate.as_view(),name='stu_add'),
+    #url(r'^add/(?P<id>\d+)/$',views.StuCreate.as_view(),name='stu_add'),
 )
