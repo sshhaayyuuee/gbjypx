3dc9865574279d05844b1e6f2c9d4afc0ce2e4b7
sshhaayyuuee <s.shayue@gmail.com>
2014-05-21 09:00:49 +0800

    删除CreateView 改用render

--- enrollment/urls.py
+++ enrollment/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 from enrollment import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.StuCreate.as_view()),
+    url(r'^$', views.StuCreate()),
     #url(r'^add/(?P<id>\d+)/$',views.StuCreate.as_view(),name='stu_add'),
 )
--- enrollment/views.py
+++ enrollment/views.py
@@ -11,32 +11,13 @@ from django.core.urlresolvers import reverse_lazy
 # Create your views here.
 from enrollment.models import Student,Course
 
+def StuCreate(request):
+      return HttpResponse("Hello world")	
+'''
 class IndexView(generic.ListView):
     template_name = 'enrollment/index.html'
     context_object_name = 'Course_list'
 
     def get_queryset(self):
         return Course.objects.all()
-
-class DetailView(generic.DetailView):
-    model = Student
-    template_name = 'enrollment/detail.html'
-	
-class StuCreate(CreateView):
-    model = Student
-    context_object_name = 'course_id'
-    fields = ['name','nation','dept','position','cellphone','workphone','course']
-    template_name = 'enrollment/stucreate.html'
-	
-	
-class StuUpdate(UpdateView):
-    model = Student
-    fields = ['name']
-	
-class AuthorDelete(DeleteView):
-    model = Student
-    success_url = reverse_lazy('Stu-list')
-	
-def stu(request,number):
-    
-    return render_to_response('stucreate.html')
+'''
